b29a89b53696240b7a2b2348e25494d27fb60133
camara9126 <acvision01@gmail.com>
2026-05-07 15:55:09 +0100

    commencement nouveau projet logiciel SaaS

--- resources/views/dashboard/depenses/index.blade.php
+++ resources/views/dashboard/depenses/index.blade.php
@@ -118,9 +118,9 @@
                                                     <select name="mode_paiement" class="form-control" required>
                                                         <option value="">-- Choisir --</option>
                                                         <option value="cash">Cash</option>
-                                                        <option value="moblie_money">Mobile Money</option>
-                                                        <option value="virement">Virement</option>
-                                                        <option value="cheque">Cheque</option>
+                                                        <option value="orange_money">Mobile Money</option>
+                                                        <option value="wave">Wave</option>
+                                                        <option value="banque">Banque</option>
                                                         <option value="autre">Autre</option>
                                                     </select>
                                                 </div>
